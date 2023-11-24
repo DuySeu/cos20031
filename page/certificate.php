ffb31ab12bfa1dcffcc52e91def6664427813867
QDuy <nguyenquocduy0207@gmail.com>
2023-11-24 10:59:14 +0700

    certidicate display data

--- page/certificate.php
+++ page/certificate.php
@@ -1,3 +1,14 @@
+<?php
+  include_once('../includes/dbConnect.php');
+  if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["courseName"])) {
+    $courseName = $_GET["courseName"];
+    $sql = "SELECT * FROM certificates WHERE course_name = '$courseName'";
+  } else {
+    $sql = "SELECT * FROM certificates LIMIT 15";
+  }
+  $result = mysqli_query($conn, $sql);
+  $resultCheck = mysqli_num_rows($result);
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,7 +30,44 @@
       </ul>
     </div>
     <div>
-      <li ><a href="certificateearned.php">Certificate Earned</a></li>
+      <li><a href="certificateearned.php">Certificate Earned</a></li>
+    </div>
+    <div class="taskbar-2">
+      <form action="certificate.php" method="get">
+        <ul class="sql-task">
+          <li><button type="submit">All</a></li>
+          <li><button type="submit" name="courseName" value="Computer Science">Computer Science</a></li>
+          <li><button type="submit" name="courseName" value="Mathematics">Math</a></li>
+          <li><button type="submit" name="courseName" value="Biology">Biology</a></li>
+          <li><button type="submit" name="courseName" value="Art">Art</a></li>
+          <li><button type="submit" name="courseName" value="Geography">Geography</a></li>
+        </ul>
+      </form>
+    </div>
+    <ul class="certificate">
+      <?php
+      if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+      ?>
+        <li class="cerificate-display">
+          <div class="logo">
+            <?php echo $row['issuing_authority']; ?>
+          </div>
+          <div class="marquee">
+            <?php echo $row['certificate_name']; ?>
+            </div>
+          <div class="assignment">
+            Validity period: <?php echo $row['validity_period']; ?> years
+          </div>
+          <div class="reason">
+            <?php echo $row['description']; ?>
+          </div>
+        </li>
+      <?php
+        }
+      }
+      ?>
+    </ul>
     </div>
   </body>
 </html>
--- page/certificateearned.php
+++ page/certificateearned.php
@@ -8,7 +8,7 @@
   <body>
     <div class="taskbar">
     <ul>
-    <li class="mucluc"><a href="../index.php">Home page</a></li>
+        <li class="mucluc"><a href="../index.php">Home page</a></li>
         <li class="mucluc"><a href="course.php">Course</a></li>
         <li class="mucluc"><a href="certificate.php">Certificate</a></li>
         <li class="mucluc"><a href="candicateprofile.php">Candicate profile</a></li>
@@ -18,5 +18,24 @@
         <li class="mucluc"><a href="login.php">Login</a></li>
       </ul>
     </div>
+    <ul>
+    <li class="cerificate-display">
+      <div class="logo">
+        An Organization
+      </div>
+      <div class="marquee">
+        Certificate of Completion
+        </div>
+      <div class="assignment">
+        Validity period
+      </div>
+      <div class="person">
+        Joe Nathan
+      </div>
+      <div class="reason">
+        For deftly defying the laws of gravity
+      </div>
+    </li>
+    </ul>
   </body>
 </html>
