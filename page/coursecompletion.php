0a56e52cab8f64c9f13db335a81d62c05e27ced0
QDuy <nguyenquocduy0207@gmail.com>
2023-11-25 12:00:09 +0700

    userreview update

--- page/coursecompletion.php
+++ page/coursecompletion.php
@@ -18,5 +18,47 @@
           <li class="mucluc"><a href="login.php">Login</a></li>
       </ul>
     </div>
+    <hr id="headerdivider">
+    <form method="post" action="">
+      <fieldset id="bigform1">
+        <legend id="legendofbigform">Course Review</legend>
+            <fieldset>
+                <legend>Personal information</legend>
+                <p>
+                    <label for="firstname">User_ID</label> 
+                    <input type="text" name="firstname" id="firstname" maxlength="25" size="50" required="required">
+                </p>
+            </fieldset>
+
+            <fieldset>
+                <legend>Course Information</legend>
+                <p>
+                    <label for="streetaddress">Course_ID</label> 
+                    <input type="text" name="streetaddress" id="streetaddress" maxlength="40" size="50" required="required">
+                </p>
+            </fieldset>
+            <fieldset>
+                <legend>Service Rating (1-5)</legend>
+                <div class="rating">
+                  <input type="radio" id="star5" name="rating" value="5" required>
+                  <label for="star5"></label>
+                  <input type="radio" id="star4" name="rating" value="4" required>
+                  <label for="star4"></label>
+                  <input type="radio" id="star3" name="rating" value="3" required>
+                  <label for="star3"></label>
+                  <input type="radio" id="star2" name="rating" value="2" required>
+                  <label for="star2"></label>
+                  <input type="radio" id="star1" name="rating" value="1" required>
+                  <label for="star1"></label>
+                </div>
+              </fieldset>
+            <fieldset>
+                <legend>User Comment</legend>
+                <textarea id="enquiry" name="enquiry" rows="5" cols="50" placeholder="Specify a particular aspect you are interested in!"></textarea>
+            </fieldset>
+            <input type= "submit" value="Submit" class="button">
+            <input type= "reset" value="Reset" class="button">
+      </fieldset>
+    </form>
   </body>
 </html>
--- page/userreview.php
+++ page/userreview.php
@@ -1,3 +1,12 @@
+<?php
+  include_once('../includes/dbConnect.php');
+  $sql = "SELECT courses.course_name, users.username, user_reviews.rating, user_reviews.review_text, user_reviews.review_date
+          FROM courses, users, user_reviews
+          WHERE courses.course_id = user_reviews.course_id
+          AND users.user_id = user_reviews.user_id";
+  $result = mysqli_query($conn, $sql);
+  $resultCheck = mysqli_num_rows($result);
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,7 +17,7 @@
   <body>
     <div class="taskbar">
       <ul>
-      <li class="mucluc"><a href="../index.php">Home page</a></li>
+          <li class="mucluc"><a href="../index.php">Home page</a></li>
           <li class="mucluc"><a href="course.php">Course</a></li>
           <li class="mucluc"><a href="certificate.php">certificate</a></li>
           <li class="mucluc"><a href="candicateprofile.php">Candicate profile</a></li>
@@ -16,6 +25,33 @@
           <li class="mucluc"><a href="User.php">User</a></li>
           <li class="mucluc"><a href="Userreview.php">User Review</a></li>
           <li class="mucluc"><a href="login.php">Login</a></li>
+      </ul>
+    </div>
+    <div class="review">
+    <table style="width: 100%;">
+          <tr>
+            <th>Review Name</th>
+            <th>Course Review</th>
+            <th>Rating</th>
+            <th>Review</th>
+            <th>Date</th>
+          </tr>
+          <?php
+            if ($resultCheck > 0) {
+              while ($row = mysqli_fetch_assoc($result)) {
+          ?>
+          <tr>
+            <td><?php echo $row['username']; ?></td>
+            <td><?php echo $row['course_name']; ?></td>
+            <td><?php echo $row['rating']; ?></td>
+            <td><?php echo $row['review_text']; ?></td>
+            <td><?php echo $row['review_date']; ?></td>
+          </tr>
+          <?php
+              }
+            }
+          ?>
+    </table>
     </div>
   </body>
 </html>
