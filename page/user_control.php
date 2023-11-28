5dd4c45222ac5c04ba6797f4436a7e4743f0b9ac
DTDtech <85300403+DTDtech@users.noreply.github.com>
2023-11-28 12:18:02 +0700

    added form to change user info

--- page/user_control.php
+++ page/user_control.php
@@ -23,35 +23,73 @@
   <div class="bcv">
     <p class="bcv-header">Edit</p>
   </div>
-    <hr id="headerdivider">
-    <form action="" method="">
-      <fieldset id="bigform2">
-        <label for="name">Name:</label><br>
-        <input type="text" name="name" id="name" required="required" placeholder="Enter new name" maxlength="25" size="50"><br><br>
-        <label for="password">Password:</label><br>
-        <input type="text" name="password" id="password" required="required" placeholder="Enter new password" maxlength="40" size="50"><br><br>
-        <label for="email">Email:</label><br>
-        <input type="text" name="email" id="email" required="required" placeholder="Enter new email" maxlength="40" size="50"><br><br>
-        <label for="role">Role:</label><br>
-        <input type="text" name="role" id="role" required="required" placeholder="Enter new role" maxlength="40" size="50"><br><br>
-        <input type="submit" value="Submit">
-        <input type="reset" value="Reset">
-      </fieldset>
-    </form>
+  <hr id="headerdivider">
+  <form action="user_control.php" method="post">
+    <fieldset id="bigform2">
+      <label for="current_name">Enter current username:</label><br>
+      <input type="text" name="current_name" id="current_name" required="required" placeholder="Enter current username" maxlength="25" size="50"><br><br>
+      <label for="name">Name:</label><br>
+      <input type="text" name="name" id="name" placeholder="Enter new username" maxlength="25" size="50"><br><br>
+      <label for="password">Password:</label><br>
+      <input type="text" name="password" id="password" placeholder="Enter new password" maxlength="40" size="50"><br><br>
+      <label for="email">Email:</label><br>
+      <input type="text" name="email" id="email" placeholder="Enter new email" maxlength="40" size="50"><br><br>
+      <label for="role">Role:</label><br>
+      <input type="text" name="role" id="role" placeholder="Enter new role" maxlength="40" size="50"><br><br>
+      <input type="submit" value="Submit">
+      <input type="reset" value="Reset">
+    </fieldset>
+  </form>
 
+  <div class="update_user_info">
   <?php
-    include_once('../includes/dbConnect.php');
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["jobID"]) && isset($_POST["userID"])) {
-      $jobID = $_POST["jobID"];
-      $userID = $_POST["userID"];
-      $sql = "INSERT INTO job_application (job_id, user_id, application_date) VALUES ('$jobID', '$userID', curdate())";
-      if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
+  include_once('../includes/dbConnect.php');
+  if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["current_name"]) && 
+  ( (isset($_POST["name"]) && ($_POST["name"] != '')) || (isset($_POST["password"]) && ($_POST["password"] != '')) || (isset($_POST["email"]) && ($_POST["email"] != '')) || (isset($_POST["role"]) && ($_POST["role"] != '')) )) {
+    $current_name = $_POST["current_name"];
+    $sql = "UPDATE users";
+    if (isset($_POST["name"]) && ($_POST["name"] != '')) {
+      $name = $_POST["name"];
+      $sql .= " SET username = '$name'";
+    }
+    if (isset($_POST["password"]) && ($_POST["password"] != '')) {
+      $password = $_POST["password"];
+      $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+      if ($sql != "UPDATE users") {
+        $sql .= ", password = '$hashed_password'";
       } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        $sql .= " SET password = '$hashed_password'";
       }
     }
-    ?>
+    if (isset($_POST["email"]) && ($_POST["email"] != '')) {
+      $email = $_POST["email"];
+      if ($sql != "UPDATE users") {
+        $sql .= ", email = '$email'";
+      } else {
+        $sql .= " SET email = '$email'";
+      }
+    } 
+    if (isset($_POST["role"]) && ($_POST["role"] != '')) {
+      $role = $_POST["role"];
+      if ($sql != "UPDATE users") {
+        $sql .= ", role = '$role'";
+      } else {
+        $sql .= " SET user_role = '$role'";
+      }
+    } 
+    $sql .= " WHERE username = '$current_name'";
+    if (mysqli_query($conn, $sql)) {
+      echo "Updated record successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  }
+  else {
+    echo "Please enter data to be changed";
+  }
+  ?>
+  </div>
 
 </body>
+
 </html>
